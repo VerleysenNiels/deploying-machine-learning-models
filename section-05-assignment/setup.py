5adf5b3bdbb887211df11900a8a7df1239847dcd
Niels Verleysen <nlsverleysen99@gmail.com>
2022-10-21 12:06:27 +0200

    Update initialization and other general files

--- section-05-assignment/setup.py
+++ section-05-assignment/setup.py
@@ -6,11 +6,11 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = 'tid-regression-model'
+NAME = 'tid-titanic-model'
 DESCRIPTION = "Example regression model package from Train In Data."
-URL = "https://github.com/trainindata/testing-and-monitoring-ml-deployments"
-EMAIL = "christopher.samiullah@protonmail.com"
-AUTHOR = "ChristopherGS"
+URL = "https://github.com/VerleysenNiels/deploying-machine-learning-models"
+EMAIL = "///"
+AUTHOR = "Niels Verleysen"
 REQUIRES_PYTHON = ">=3.6.0"
 
 
@@ -25,7 +25,7 @@ long_description = DESCRIPTION
 about = {}
 ROOT_DIR = Path(__file__).resolve().parent
 REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
-PACKAGE_DIR = ROOT_DIR / 'regression_model'
+PACKAGE_DIR = ROOT_DIR / 'titanic_model'
 with open(PACKAGE_DIR / "VERSION") as f:
     _version = f.read().strip()
     about["__version__"] = _version
